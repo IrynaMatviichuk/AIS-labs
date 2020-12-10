e3b0f6bc5012c248aab43ef72ef6c9f8f8b8f5dc
Iryna Matviichuk <31668203+IraMatviichuk@users.noreply.github.com>
2020-12-10 21:58:52 +0200

    update statistics info (#5)

--- lab2/a_star_algorithm.py
+++ lab2/a_star_algorithm.py
@@ -4,23 +4,34 @@ from Node import Node
 
 
 def a_star(start, goal, comparator):
+    number_of_nodes_visited = 0
+    max_number_of_nodes = 0
+    number_of_nodes = 0
+    number_of_explored = 0
     nodes = []
-    nodes.append(Node.create_node(start, None, None, 0))
     explored = []
-    count = 0
+    nodes.append(Node.create_node(start, None, None, 0))
     while nodes:
         nodes.sort(key=functools.cmp_to_key(comparator.cmp))
         node = nodes.pop(0)
         explored.append(node.get_state())
-        count += 1
+        number_of_nodes_visited += 1
         print("trying state", node.state.get_matrix(), " and move: ", node.operator)
+        if len(nodes) + len(explored) > max_number_of_nodes:
+            max_number_of_nodes = len(nodes) + len(explored)
+            number_of_nodes = len(nodes)
+            number_of_explored = len(explored)
+
         if node.state == goal:
-            print("The number of nodes visited", count)
-            print("States of moves are as follows:")
-            return node.path_from_start()
+            return {
+                "max_number_of_nodes": max_number_of_nodes,
+                "number_of_nodes": number_of_nodes,
+                "number_of_explored": number_of_explored,
+                "number_of_nodes_visited": number_of_nodes_visited,
+                "path_from_start": node.path_from_start(),
+            }
         else:
             expanded_nodes = Node.expand_node(node)
             for item in expanded_nodes:
-                state = item.get_state()
-                if state not in explored:
+                if item.get_state() not in explored:
                     nodes.append(item)
